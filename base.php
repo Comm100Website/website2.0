a05bf9bd5ad5dc579d146a91131811b4513d296f
Comm100Website <mark@comm100.com>
2018-10-23 17:10:48 +0800

    add landing page of free trial

--- base.php
+++ base.php
@@ -9,12 +9,6 @@ use Roots\Sage\Wrapper;
     <!--[if IE]><span class="ie7note">You are using an <strong>outdated</strong> browser. Please <a href="//browsehappy.com/">upgrade your browser</a> to improve your experience.</span><![endif]--><!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHPR23J" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
-    <?php if ($post && $post->post_type == 'kbe_knowledgebase'): ?>
-        <div class="notify hidden-xs" style="display: block;">
-            <a href="/privacy/">We have updated our Privacy Policy. Check it out here &gt;&gt;</a>
-            <span class="close">×</span>
-        </div>
-    <?php endif; ?>
     <?php
       do_action('get_header');
     ?>
