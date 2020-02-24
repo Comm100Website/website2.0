0018f3d05689dee6dc97e55cb199550debf2b68f
Martin Blumenfeld <martin@rps.is>
2020-02-24 12:46:09 -0800

    Add LMS course author template

--- lifterlms/default-templates/course/author.php
+++ lifterlms/default-templates/course/author.php
@@ -19,6 +19,30 @@ if ( ! $instructors ) {
 $count = count( $instructors );
 ?>
 
+                                <!-- begin author -->
+                                <?php
+                                $coauthors = get_coauthors();
+                                foreach( $coauthors as $coauthor ):
+                                    $archive_link = get_author_posts_url( $coauthor->ID, $coauthor->user_nicename );
+                                    // $userdata = get_userdata( $coauthor->ID );
+                                ?>
+                                    <div class="authorsure-author-box clearfix">
+                                        <div class="author-thumb"><?php echo get_avatar( $coauthor, 100 ); ?></div>
+                                        <div class="author-details">
+                                            <h4>
+                                                About <a href="<?php echo $archive_link; ?>">
+                                                    <?php echo $coauthor->display_name; ?>
+                                                </a>
+                                            </h4>
+                                            <p><?php echo $coauthor->description; ?></p>
+                                        </div>
+                                    </div>
+                                <?php
+                                endforeach;
+                                ?>
+                                <!-- end author -->
+
+
 <section class="llms-instructor-info">
 	<h3 class="llms-meta-title"><?php echo _n( 'Course Instructor', 'Course Instructors', count( $instructors ), 'lifterlms' ); ?></h3>
 	<div class="llms-instructors llms-cols">
